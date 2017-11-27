4ef18f9381979681f491b602bd8b3259297b4a6f
BrianLM <brian.distefano@libertymutual.com>
2017-11-27 13:10:28 -0500

    Fix user returns
    Fixes the returns from User
    Removed unneeded actions from User Profile controller
    Adds routes back to routes.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :user_profile
+  attributes :id, :email, :user_profile, :exploration, :creature
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,15 @@ Rails.application.routes.draw do
   # resources :creatures
   # resources :items
   # resources :jobs
-  # resources :explorations
-  # resources :levels
-  resources :user_profiles
+  resources :explorations
+  resources :levels, only: [:index]
+  resources :user_profiles, only: [:show]
   resources :examples, except: [:new, :edit]
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
+  # post '/authentication/signin' => 'users#signin' << POC nested route
   delete '/sign-out/:id' => 'users#signout'
   patch '/change-password/:id' => 'users#changepw'
+  patch '/profile/:id' => 'user_profiles#show'
   resources :users, only: [:index, :show]
 end
